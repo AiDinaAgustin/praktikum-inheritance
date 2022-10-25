56f86091229b6a9223b76e3bb85c60e146179a50
AiDinaAgustin <95268109+AiDinaAgustin@users.noreply.github.com>
2022-10-25 09:36:55 +0700

    Add files via upload

---
+++ Percobaan5/HappyObject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Percobaan5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class HappyObject extends MoodyObject{
+protected String getMood(){
+return"happy";
+}
+public void laugh(){
+    System.out.println("Hahahaha");
+}
+}
